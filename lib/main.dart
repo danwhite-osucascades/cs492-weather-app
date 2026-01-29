bf951d256377a0ad8e452efd7766ac80afe61c5f
Dan <dan.white@osucascades.edu>
2026-01-29 14:45:02 -0800

    halfway

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weatherapp/providers/location_provider.dart';
 import 'package:weatherapp/widgets/forecast.dart';
 import './models/forecast.dart';
 import './models/location.dart';
@@ -7,8 +9,12 @@ import './widgets/location.dart';
 // TODOs
 // Implement global state management for Location and Forecast using ChangeProvider
 
+// TODO:
+// Update the location widget, so that you no longer need to pass the location or set functions
+
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => LocationProvider(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -38,8 +44,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   List<Forecast> _forecasts = [];
   Forecast? _activeForecast;
-  Location? _location;
+  // Location? _location;
   late final TabController _tabController;
+  final LocationProvider _locationProvider = LocationProvider();
 
   @override
   void initState() {
@@ -47,36 +54,18 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     _tabController = TabController(length: 2, vsync: this);
     _tabController.index = 1;
     _tabController.addListener(() {
-      if (_location == null) {
+      if (_locationProvider.location == null) {
         _tabController.index = 1;
       }
+    });
+    _locationProvider.addListener(() {
+      _getForecasts(_locationProvider.location);
     });
   }
 
   void _setActiveForecast(Forecast forecast) {
     setState(() {
       _activeForecast = forecast;
-    });
-  }
-
-  void _setLocationFromGps() async {
-    Location location = await getLocationFromGps();
-    _getForecasts(location);
-    setState(() {
-      _location = location;
-    });
-  }
-
-  void _setLocation(String? locationString) async {
-    Location? location;
-
-    if (locationString != null && locationString.trim().isNotEmpty) {
-      location = await getLocationFromString(locationString);
-      _getForecasts(location);
-    }
-
-    setState(() {
-      _location = location;
     });
   }
 
@@ -98,11 +87,11 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
           actions: [
-            if (_location != null)
+            if (_locationProvider.location != null)
               Padding(
                 padding: const EdgeInsets.only(right: 16.0),
                 child: Text(
-                  "${_location!.city}, ${_location!.state}",
+                  "${_locationProvider.location!.city}, ${_locationProvider.location!.state}",
                   style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                         fontWeight: FontWeight.w500,
                         letterSpacing: 0.5,
@@ -127,9 +116,9 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                 activeForecast: _activeForecast,
                 setActiveForecast: _setActiveForecast),
             LocationWidget(
-              location: _location,
-              setLocation: _setLocation,
-              setLocationFromGps: _setLocationFromGps,
+              location: _locationProvider.location,
+              setLocation: _locationProvider.setLocation,
+              setLocationFromGps: _locationProvider.setLocationFromGps,
             ),
           ],
         ),
