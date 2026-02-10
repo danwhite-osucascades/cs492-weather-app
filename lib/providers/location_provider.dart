26a2ac8aa393eb08ed86134bb0123d50e50bb76c
Dan <dan.white@osucascades.edu>
2026-02-10 14:48:31 -0800

    midpoint

--- lib/providers/location_provider.dart
+++ lib/providers/location_provider.dart
@@ -5,18 +5,30 @@ import 'package:weatherapp/models/location.dart';
 class LocationProvider extends ChangeNotifier {
   Location? location;
 
+  Map<String, Location> savedLocations = {};
+
   void setLocationFromGps() async {
     location = await getLocationFromGps();
+
+    if (location != null && !savedLocations.containsKey(location!.zip)) {
+      savedLocations[location!.zip] = location!;
+    }
+
     notifyListeners();
   }
 
   void setLocation(String? locationString) async {
     if (locationString != null && locationString.trim().isNotEmpty) {
       location = await getLocationFromString(locationString);
-    }
-    else {
+    } else {
       location = null;
     }
+
+    if (location != null && !savedLocations.containsKey(location!.zip)) {
+      savedLocations[location!.zip] = location!;
+    }
+
     notifyListeners();
   }
+
 }
