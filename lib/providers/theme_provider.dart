fa2f441a83613605be1a4360980ca979be622244
Dan <dan.white@osucascades.edu>
2026-02-12 15:10:21 -0800

    pre todos

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  bool darkMode = true;
 
-  void setDarkMode(bool mode){
+  bool darkMode = false;
+
+  void loadDarkModePrefs() async {
+    final prefs = SharedPreferencesAsync();
+    bool? mode = await prefs.getBool("darkMode");
+    setDarkMode(mode!);
+    }
+
+  void setDarkMode(bool mode) async {
     darkMode = mode;
     notifyListeners();
+    final prefs = SharedPreferencesAsync();
+    await prefs.setBool("darkMode", mode);
   }
 }
